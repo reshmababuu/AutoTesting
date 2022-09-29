238959c5538299ec15658aebf033dde28ac0cea1
user <user@DESKTOP-0F5314O.bbrouter>
2022-09-29 20:54:54 +0530

    first commit

---
+++ AutoTesting/src/main/java/DoubleSum.java
@@ -0,0 +1,12 @@
+
+public class DoubleSum {
+	public static void main(String [] args)
+	{
+	double a = 8.2356;
+	double b = 5.3568;
+	double c = 2.3569;
+	double sum = a + b + c;
+	System.out.println("The sum is " +sum);
+	}
+
+}
---
+++ AutoTesting/src/main/java/MethodsDemo.java
@@ -0,0 +1,13 @@
+
+public class MethodsDemo {
+	public static void main(String[] args)
+	{
+		MethodsDemo md = new MethodsDemo();  //object create
+	md.doLogin(); // instance method calling
+	}
+	public void doLogin() // instance method definition(should be return outside the main functn, inside the class)
+	{
+	System.out.println("hai");
+	}
+
+}
