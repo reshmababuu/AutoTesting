19992294f7475954bd87f49a0c265cce349f0e63
user <user@DESKTOP-0F5314O.bbrouter>
2022-09-29 21:01:34 +0530

    third commit

---
+++ AutoTesting/EncapsRev2.java
@@ -0,0 +1,17 @@
+package Revision_oops_concepts;
+
+public class EncapsRev2 {
+
+	public static void main(String[] args) {
+		
+		EncpasRev obj = new EncpasRev();
+		obj.setAge(28);
+		obj.setNo(10);
+		obj.setName("Reshma");
+		System.out.println("name "+obj.getName());
+		System.out.println("no "+obj.getNo());
+		System.out.println("age "+obj.getAge());
+
+	}
+
+}
